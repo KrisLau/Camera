21295169028ee1f132b50bda5c0a653fb9555da4
KrisLau <kristinlaumayhui@gmail.com>
2021-01-26 11:55:04 -0600

    Add project files.

---
+++ Camera Record/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Camera_Record
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pbCapturedImage = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.cboVideoSources = new System.Windows.Forms.ComboBox();
+            this.vspWebcam = new AForge.Controls.VideoSourcePlayer();
+            this.lblCapturedImage = new System.Windows.Forms.Label();
+            this.lblWebcam = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCapturedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(433, 142);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(144, 33);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pbCapturedImage
+            // 
+            this.pbCapturedImage.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pbCapturedImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCapturedImage.Location = new System.Drawing.Point(591, 87);
+            this.pbCapturedImage.Margin = new System.Windows.Forms.Padding(4);
+            this.pbCapturedImage.Name = "pbCapturedImage";
+            this.pbCapturedImage.Size = new System.Drawing.Size(409, 281);
+            this.pbCapturedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCapturedImage.TabIndex = 2;
+            this.pbCapturedImage.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(433, 212);
+            this.btnCapture.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(144, 33);
+            this.btnCapture.TabIndex = 3;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // cboVideoSources
+            // 
+            this.cboVideoSources.FormattingEnabled = true;
+            this.cboVideoSources.Location = new System.Drawing.Point(433, 87);
+            this.cboVideoSources.Margin = new System.Windows.Forms.Padding(4);
+            this.cboVideoSources.Name = "cboVideoSources";
+            this.cboVideoSources.Size = new System.Drawing.Size(148, 24);
+            this.cboVideoSources.TabIndex = 4;
+            // 
+            // vspWebcam
+            // 
+            this.vspWebcam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vspWebcam.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.vspWebcam.Location = new System.Drawing.Point(16, 87);
+            this.vspWebcam.Margin = new System.Windows.Forms.Padding(4);
+            this.vspWebcam.Name = "vspWebcam";
+            this.vspWebcam.Size = new System.Drawing.Size(409, 282);
+            this.vspWebcam.TabIndex = 5;
+            this.vspWebcam.VideoSource = null;
+            this.vspWebcam.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.vspWebcam_NewFrame);
+            // 
+            // lblCapturedImage
+            // 
+            this.lblCapturedImage.AutoSize = true;
+            this.lblCapturedImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCapturedImage.Location = new System.Drawing.Point(587, 36);
+            this.lblCapturedImage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCapturedImage.Name = "lblCapturedImage";
+            this.lblCapturedImage.Size = new System.Drawing.Size(141, 20);
+            this.lblCapturedImage.TabIndex = 6;
+            this.lblCapturedImage.Text = "Captured Image";
+            // 
+            // lblWebcam
+            // 
+            this.lblWebcam.AutoSize = true;
+            this.lblWebcam.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWebcam.Location = new System.Drawing.Point(28, 36);
+            this.lblWebcam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWebcam.Name = "lblWebcam";
+            this.lblWebcam.Size = new System.Drawing.Size(81, 20);
+            this.lblWebcam.TabIndex = 7;
+            this.lblWebcam.Text = "Webcam";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 393);
+            this.Controls.Add(this.lblWebcam);
+            this.Controls.Add(this.lblCapturedImage);
+            this.Controls.Add(this.vspWebcam);
+            this.Controls.Add(this.cboVideoSources);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.pbCapturedImage);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCapturedImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pbCapturedImage;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.ComboBox cboVideoSources;
+        private AForge.Controls.VideoSourcePlayer vspWebcam;
+        private System.Windows.Forms.Label lblCapturedImage;
+        private System.Windows.Forms.Label lblWebcam;
+    }
+}
+
---
+++ Camera Record/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Text.RegularExpressions;
+using AForge.Video;
+using System.Diagnostics;
+using AForge.Video.DirectShow;
+using System.Collections;
+using System.IO;
+using System.Drawing.Imaging;
+using System.IO.Ports;
+using System.Globalization;
+using System.Net;
+
+namespace Camera_Record
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch m_Stopwatch = null;
+        private FilterInfoCollection m_VideoDevices;
+        private VideoCaptureDevice m_VideoDevice;
+        private VideoCapabilities[] m_SnapshotCapabilities;
+        private ArrayList m_ListCamera = new ArrayList();
+        public string m_PathFolder = Application.StartupPath + @"\DrugImageCapture\";
+        private static bool m_NeedSnapshot = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getCameraList();
+        }
+
+        private static string m_UsbCamera;
+        public string UsbCamera
+        {
+            get { return m_UsbCamera; }
+            set { m_UsbCamera = value; }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            OpenCamera();
+        }
+
+        #region Open Scan Camera
+        private void OpenCamera()
+        {
+            try
+            {
+                UsbCamera = cboVideoSources.SelectedIndex.ToString();
+                m_VideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (m_VideoDevices.Count != 0)
+                {
+                    // add all devices to combo
+                    foreach (FilterInfo device in m_VideoDevices)
+                    {
+                        m_ListCamera.Add(device.Name);
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Camera devices found");
+                }
+
+                m_VideoDevice = new VideoCaptureDevice(m_VideoDevices[Convert.ToInt32(UsbCamera)].MonikerString);
+                m_SnapshotCapabilities = m_VideoDevice.SnapshotCapabilities;
+                if (m_SnapshotCapabilities.Length == 0)
+                {
+                    MessageBox.Show("Camera capture not supported");
+                }
+
+                OpenVideoSource(m_VideoDevice);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+        }
+        #endregion
+
+
+        //Delegate for capture, insert database, update picturebox 
+        public delegate void CaptureSnapshot(Bitmap image);
+        public void UpdateCaptureSnapshot(Bitmap image)
+        {
+            try
+            {
+                m_NeedSnapshot = false;
+                pbCapturedImage.Image = image;
+                pbCapturedImage.Update();
+
+               
+                string imageName = "sampleImage";
+                string captureName = imageName + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".bmp";
+
+                if (Directory.Exists(m_PathFolder))
+                {
+                    pbCapturedImage.Image.Save(m_PathFolder + captureName, ImageFormat.Bmp);
+                }
+                else
+                {
+                    Directory.CreateDirectory(m_PathFolder);
+                    pbCapturedImage.Image.Save(m_PathFolder + captureName, ImageFormat.Bmp);
+                }
+            }
+
+            catch { }
+
+        }
+
+        public void OpenVideoSource(IVideoSource source)
+        {
+            try
+            {
+                // set busy cursor
+                this.Cursor = Cursors.WaitCursor;
+
+                // stop current video source
+                CloseCurrentVideoSource();
+
+                // start new video source
+                vspWebcam.VideoSource = source;
+                vspWebcam.Start();
+
+                // reset stop watch
+                m_Stopwatch = null;
+
+
+                this.Cursor = Cursors.Default;
+            }
+            catch { }
+        }
+
+        private void getCameraList()
+        {
+            m_VideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (m_VideoDevices.Count != 0)
+            {
+                // add all devices to combo
+                foreach (FilterInfo device in m_VideoDevices)
+                {
+                    cboVideoSources.Items.Add(device.Name);
+
+                }
+            }
+            else
+            {
+                cboVideoSources.Items.Add("No devices found");
+            }
+
+            cboVideoSources.SelectedIndex = 0;
+        }
+
+        public void CloseCurrentVideoSource()
+        {
+            try
+            {
+
+                if (vspWebcam.VideoSource != null)
+                {
+                    vspWebcam.SignalToStop();
+
+                    // wait ~ 3 seconds
+                    for (int i = 0; i < 30; i++)
+                    {
+                        if (!vspWebcam.IsRunning)
+                            break;
+                        System.Threading.Thread.Sleep(100);
+                    }
+
+                    if (vspWebcam.IsRunning)
+                    {
+                        vspWebcam.Stop();
+                    }
+
+                    vspWebcam.VideoSource = null;
+                }
+            }
+            catch { }
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            m_NeedSnapshot = true;
+        }
+
+        private void vspWebcam_NewFrame(object sender, ref Bitmap image)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                Graphics g = Graphics.FromImage(image);
+
+                // paint current time
+                SolidBrush brush = new SolidBrush(Color.Red);
+                g.DrawString(now.ToString(), this.Font, brush, new PointF(5, 5));
+                brush.Dispose();
+
+                if (m_NeedSnapshot)
+                {
+                    this.Invoke(new CaptureSnapshot(UpdateCaptureSnapshot), image);
+                }
+                g.Dispose();
+            }
+            catch
+            { }
+        }
+    }
+}
